76eb9d69a59adaa139448b38f7ed56d3deccda54
abdulrahman <abdulrahmanramadan22@gmail.com>
2024-01-21 05:12:15 +0200

    - added Development & Productions flavors for Android. - changed Android app name for each flavor. - added launch.json for flavors config. - added two main files for each flavor.

---
+++ lib/main_development.dart
@@ -0,0 +1,28 @@
+import 'package:docdoc/core/di/dependency_injection.dart';
+import 'package:flutter/services.dart';
+
+import 'core/routing/app_router.dart';
+import 'doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+void main() async{
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.white,
+    statusBarIconBrightness:
+        Brightness.dark, //<-- For Android SEE HERE (dark icons)
+  ));
+  setupGetIt();
+    // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
+
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
+
+
+
+
+
